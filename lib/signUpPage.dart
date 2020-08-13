c86c80f6252d51fd712aba07b4d3d7a58a75539f
anmolseth06 <56560420+anmolseth06@users.noreply.github.com>
2020-08-13 14:35:34 +0530

    Update signUpPage.dart

--- lib/signUpPage.dart
+++ lib/signUpPage.dart
@@ -9,142 +9,171 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Text(
-            "Phone Auth demo📱",
-            style: TextStyle(
-              color: Colors.cyan,
-              fontSize: 30,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          Image.network(
-            "https://avatars1.githubusercontent.com/u/41328571?s=280&v=4",
-            height: 150,
-          ),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: TextField(
-              keyboardType: TextInputType.phone,
-              decoration: new InputDecoration(
+  String phoneNumber, verificationId;
+  String otp, authStatus = "";
+
+  Future<void> verifyPhoneNumber(BuildContext context) async {
+    await FirebaseAuth.instance.verifyPhoneNumber(
+      phoneNumber: phoneNumber,
+      timeout: const Duration(seconds: 15),
+      verificationCompleted: (AuthCredential authCredential) {
+        setState(() {
+          authStatus = "Your account is successfully verified";
+        });
+      },
+      verificationFailed: (AuthException authException) {
+        setState(() {
+          authStatus = "Authentication failed";
+        });
+      },
+      codeSent: (String verId, [int forceCodeResent]) {
+        verificationId = verId;
+        setState(() {
+          authStatus = "OTP has been successfully send";
+        });
+        otpDialogBox(context).then((value) {});
+      },
+      codeAutoRetrievalTimeout: (String verId) {
+        verificationId = verId;
+        setState(() {
+          authStatus = "TIMEOUT";
+        });
+      },
+    );
+  }
+
+  otpDialogBox(BuildContext context) {
+    return showDialog(
+        context: context,
+        barrierDismissible: false,
+        builder: (BuildContext context) {
+          return new AlertDialog(
+            title: Text('Enter your OTP'),
+            content: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextFormField(
+                decoration: InputDecoration(
                   border: new OutlineInputBorder(
                     borderRadius: const BorderRadius.all(
                       const Radius.circular(30),
                     ),
                   ),
-                  filled: true,
-                  prefixIcon: Icon(
-                    Icons.phone_iphone,
-                    color: Colors.cyan,
-                  ),
-                  hintStyle: new TextStyle(color: Colors.grey[800]),
-                  hintText: "Enter Your Phone Number...",
-                  fillColor: Colors.white70),
-              onChanged: (value) {
-                phoneNumber = value;
-              },
+                ),
+                onChanged: (value) {
+                  otp = value;
+                },
+              ),
             ),
-          ),
-          SizedBox(
-            height: 10.0,
-          ),
-          RaisedButton(
-            shape:
-                RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
-            onPressed: () =>
-                phoneNumber == null ? null : verifyPhoneNumber(context),
-            child: Text(
-              "Generate OTP",
-              style: TextStyle(color: Colors.white),
+            contentPadding: EdgeInsets.all(10.0),
+            actions: <Widget>[
+              FlatButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                  signIn(otp);
+                },
+                child: Text(
+                  'Submit',
+                ),
+              ),
+            ],
+          );
+        });
+  }
+
+  Future<void> signIn(String otp) async {
+    await FirebaseAuth.instance
+        .signInWithCredential(PhoneAuthProvider.getCredential(
+      verificationId: verificationId,
+      smsCode: otp,
+    ));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.2,
             ),
-            elevation: 7.0,
-            color: Colors.cyan,
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          Text("Need Help?")
-        ],
+            Text(
+              "Phone Auth demo📱",
+              style: TextStyle(
+                color: Colors.cyan,
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Image.network(
+              "https://avatars1.githubusercontent.com/u/41328571?s=280&v=4",
+              height: 150,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextField(
+                keyboardType: TextInputType.phone,
+                decoration: new InputDecoration(
+                    border: new OutlineInputBorder(
+                      borderRadius: const BorderRadius.all(
+                        const Radius.circular(30),
+                      ),
+                    ),
+                    filled: true,
+                    prefixIcon: Icon(
+                      Icons.phone_iphone,
+                      color: Colors.cyan,
+                    ),
+                    hintStyle: new TextStyle(color: Colors.grey[800]),
+                    hintText: "Enter Your Phone Number...",
+                    fillColor: Colors.white70),
+                onChanged: (value) {
+                  phoneNumber = value;
+                },
+              ),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            RaisedButton(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30)),
+              onPressed: () =>
+                  phoneNumber == null ? null : verifyPhoneNumber(context),
+              child: Text(
+                "Generate OTP",
+                style: TextStyle(color: Colors.white),
+              ),
+              elevation: 7.0,
+              color: Colors.cyan,
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text("Need Help?"),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              "Please enter the phone number followed by country code",
+              style: TextStyle(color: Colors.green),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              authStatus == "" ? "" : authStatus,
+              style: TextStyle(
+                  color: authStatus.contains("fail") ||
+                          authStatus.contains("TIMEOUT")
+                      ? Colors.red
+                      : Colors.green),
+            )
+          ],
+        ),
       ),
     );
   }
 }
 
-String phoneNumber, verificationId;
-String otp;
-
-Future<void> verifyPhoneNumber(BuildContext context) async {
-  final PhoneCodeAutoRetrievalTimeout autoRetrieve = (String verId) {
-    verificationId = verId;
-  };
-  final PhoneCodeSent smsCodeSent = (String verId, [int forceCodeResent]) {
-    verificationId = verId;
-    otpDialogBox(context).then((value) {
-      print("Code Sent");
-    });
-  };
-  final PhoneVerificationCompleted verifiedSuccess = (AuthCredential auth) {};
-  final PhoneVerificationFailed verifyFailed = (AuthException e) {
-    print('${e.message}');
-  };
-  await FirebaseAuth.instance.verifyPhoneNumber(
-    phoneNumber: phoneNumber,
-    timeout: const Duration(seconds: 15),
-    verificationCompleted: verifiedSuccess,
-    verificationFailed: verifyFailed,
-    codeSent: smsCodeSent,
-    codeAutoRetrievalTimeout: autoRetrieve,
-  );
-}
-
-otpDialogBox(BuildContext context) {
-  return showDialog(
-      context: context,
-      barrierDismissible: false,
-      builder: (BuildContext context) {
-        return new AlertDialog(
-          title: Text('Enter your OTP'),
-          content: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextFormField(
-              decoration: InputDecoration(
-                border: new OutlineInputBorder(
-                  borderRadius: const BorderRadius.all(
-                    const Radius.circular(30),
-                  ),
-                ),
-              ),
-              onChanged: (value) {
-                otp = value;
-              },
-            ),
-          ),
-          contentPadding: EdgeInsets.all(10.0),
-          actions: <Widget>[
-            FlatButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-                signIn(otp);
-              },
-              child: Text(
-                'Submit',
-              ),
-            ),
-          ],
-        );
-      });
-}
-
-Future<void> signIn(String otp) async {
-  final AuthCredential credential = PhoneAuthProvider.getCredential(
-    verificationId: verificationId,
-    smsCode: otp,
-  );
-
-  await FirebaseAuth.instance.signInWithCredential(credential);
-}
